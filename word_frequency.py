a7b7692922db3c25966f717530785f969afce229
esantb <e.saint.bou@gmail.com>
2023-02-15 15:15:23 -0600

    looping through text file  to show word count

--- word_frequency.py
+++ word_frequency.py
@@ -2,7 +2,7 @@ import string
 
 STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for',
               'from', 'has', 'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that',
-              'the', 'to', 'were', 'will', 'with']
+              'the', 'to', 'were', 'will', 'with', 'if', 'we', 'she']
 
 
 def remove_punctuation(words):
@@ -10,9 +10,8 @@ def remove_punctuation(words):
     stripped_file = words.translate(str.maketrans('', '', string.punctuation))
     return stripped_file
 
+
 # 4. remove "stop words" - words used so frequently we ignore
-
-
 def remove_stop_words(word_list):
     cleaned_list = []
     for word in word_list:
@@ -20,9 +19,8 @@ def remove_stop_words(word_list):
             cleaned_list.append(word)
     return cleaned_list
 
+
 # 1. use 'open' to read a text file
-
-
 def open_file(file):
     with open(file) as opened_file:
         read_file = opened_file.read()
@@ -33,17 +31,28 @@ def open_file(file):
     # calling the removal of stop words
     cleaned_list = remove_stop_words(word_list)
     print(cleaned_list)
+    return cleaned_list
 
 
 # 5. count frequency of words in a file
+# loop through the list of words, and updated the dictionary to indicate how many of each we have
+def print_word_freq(file):
+    words_to_count = open_file(file)
+    word_count = {
+        'new': words_to_count.count('new')
+    }
+    for word in words_to_count:
+        if word in word_count.keys():
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    print(word_count)
+
 
 # 6. display a count in the console in descending frequency
 
 
-def print_word_freq(file):
-    open_file(file)
-# loop through the list of words, and update
-
+# 7. turn numbers into *
 
 if __name__ == "__main__":
     import argparse
